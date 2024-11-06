e87a5eb6d59ceb19c104e57a051f66edf4acad0f
rhakagesa <rhakagesa@gmail.com>
2024-11-06 16:54:38 +0700

    reafactor hero and seputar masjid

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,55 +1,53 @@
-import { Link } from "@remix-run/react";
-
 type HeroProps = {
   fotoImg: string;
 };
 
 export default function Hero({ fotoImg }: HeroProps) {
   return (
-    <section className="w-full min-h-max rounded-b-3xl bg-gradient-to-l from-[#247EB0] to-[#134561] pb-6">
-      <div className="flex items-center pt-32 justify-between px-28 max-lg:flex-col max-lg:px-10 max-lg:pt-32">
-        {/* Text Section */}
-        <div className="w-1/2 max-lg:w-full max-lg:text-center">
-          <h6 className="text-xl font-semibold text-white mb-4 max-lg:text-lg max-md:text-md">
-            SELAMAT DATANG DI WEBSITE
-          </h6>
+    <section className="w-full min-h-max rounded-b-3xl bg-gradient-to-l from-[#247EB0] to-[#134561]">
+      {/* Content Section */}
+      <div className="flex flex-col items-center px-32 pt-32 pb-10 max-md:px-10">
+        {/* Hero Section */}
+        <div className="flex justify-between items-center mb-10 max-md:flex-col max-md:text-center">
+          {/* Text Section */}
+          <div className="w-1/2 max-xl:w-3/5 max-md:w-full">
+            <h6 className="text-3xl font-semibold text-white mb-4 max-xl:text-xl max-md:text-md">
+              SELAMAT DATANG DI WEBSITE
+            </h6>
 
-          <h1 className="text-6xl font-bold text-white max-lg:text-5xl max-md:text-4xl">
-            MASJID ABU DZAR
-          </h1>
-          <h1 className="text-6xl font-bold text-white mb-4 max-lg:text-5xl max-md:text-4xl">
-            AL - GHIFARI
-          </h1>
+            <div className="text-7xl font-bold text-white max-xl:text-5xl max-lg:text-3xl">
+              <h1>MASJID ABU DZAR</h1>
+              <h1>AL - GHIFARI</h1>
+            </div>
 
-          <h6 className="text-lg text-white font-medium max-lg:text-md max-md:text-sm">
-            Website ini merupakan sebuah ikhtiar kami untuk menjelaskan beberapa
-            hal seputar yang ada di Masjid Abu Dzar Al-Ghifari.
-          </h6>
+            <h6 className="text-2xl text-white max-xl:text-xl max-lg:text-sm">
+              Website ini merupakan sebuah ikhtiar kami untuk menjelaskan
+              beberapa hal seputar yang ada di{" "}
+              <span className="font-bold">Masjid Abu Dzar Al-Ghifari</span>.
+            </h6>
+          </div>
+
+          {/* Img Section */}
+
+          <img
+            src={fotoImg}
+            alt="foto"
+            className="w-1/2 max-xl:w-2/5 max-md:mt-5"
+          />
         </div>
 
-        {/* Img Section */}
-
-        <img
-          src={fotoImg}
-          alt="foto"
-          className="w-1/2 max-lg:w-2/3 max-lg:pt-10"
-        />
-      </div>
-
-      <div className="flex mx-52 my-10 py-5 rounded-[60px] border-2 bg-slate-400 items-center justify-center max-xl:mx-14 max-xl:py-3 max-lg:flex-col max-lg:mx-36 max-lg:text-center max-lg:rounded-3xl max-md:mx-16 max-md:py-2">
-        <p className="text-xl text-white font-semibold max-xl:text-lg max-lg:text-md max-md:text-sm">
-          Mari investasi akhirat dengan cara donasi untuk program kegiatan
-          masjid
-        </p>
-
-        <Link
-          to="#"
-          className="ml-4 px-8 py-3 text-xl rounded-[60px] bg-[#357BA2] text-white font-bold max-xl:text-lg max-lg:text-md max-lg:ml-0 max-lg:px-6 max-lg:py-2 max-lg:mt-2 max-md:text-sm max-md:px-4 max-md:py-1"
-        >
-          Donasi
-        </Link>
+        <div className="flex p-8 max-xl:p-6 max-md:p-2 rounded-[60px] border-2 bg-gradient-to-r from-slate-500 to-slate-400 items-center justify-between max-md:rounded-2xl ">
+          <div className="text-xl text-white group-hover:text-slate-900 font-bold max-xl:text-sm max-lg:text-center max-lg:leading-loose max-md:text-center max-md:leading-normal max-md:font-xs max-md:font-medium">
+            <p>
+              Mari investasi akhirat dengan cara donasi untuk program kegiatan
+              masjid
+              <span className="ml-4 rounded-[60px] bg-[#357BA2] px-6 py-2 max-lg:px-4 max-lg:py-1 max-md:px-2 max-md:py-1 max-lg:ml-2 max-md:rounded-md ">
+                Donasi
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
-    // Background
   );
 }
